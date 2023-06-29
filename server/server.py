4f5462810d42197501706a70150473c4e1429b30
Suraj Kumar Turi <surajkumarturi2000@gmail.com>
2023-06-29 16:40:01 +0530

    CORS error resolved

--- server/server.py
+++ server/server.py
@@ -1,7 +1,16 @@
+
+from flask_cors import CORS  # Import CORS from flask_cors
 from flask import Flask, request, jsonify
 import util
 
+
+# CORS policy error resolved
 app = Flask(__name__)
+CORS(app)  # Add CORS middleware to your Flask app
+
+
+app = Flask(__name__)
+
 
 
 @app.route('/classify_image', methods=['GET', 'POST'])
@@ -10,7 +19,12 @@ def classify_image():
 
     response = jsonify(util.classify_image(image_data))
 
+
     response.headers.add('Access-Control-Allow-Origin', '*')
+
+    # new lines
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST')
 
     return response
 
--- server/util.py
+++ server/util.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 
     print(classify_image(get_b64_test_image_for_virat(), None))
 
-    #print(classify_image(None, "./test_images/federer1.jpg"))
+    print(classify_image(None, "./test_images/federer1.jpg"))
     #print(classify_image(None, "./test_images/federer2.jpg"))
     #print(classify_image(None, "./test_images/virat1.jpg"))
     # print(classify_image(None, "./test_images/virat2.jpg"))
